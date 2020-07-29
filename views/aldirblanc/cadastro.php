285f743f6e627896d28de6424122b222367b9fd4
Cátia Kitahara <contato@catiakitahara.com.br>
2020-07-29 12:37:27 -0300

    primeira tela após o login do plugin

--- views/aldirblanc/cadastro.php
+++ views/aldirblanc/cadastro.php
@@ -2,6 +2,38 @@
 use MapasCulturais\i; 
 
 ?>
-
-<a href="<?= $this->controller->createUrl( 'tipo', ['individual'] ) ?>" class="btn btn-primary"> <?php i::_e('Auxílio para trabalhador da Cultura') ?> </a>
-<a href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>" class="btn btn-primary"> <?php i::_e('Auxílio para Grupos ou Espaços Culturais') ?> </a>
+<section class="lab-form-intro">
+    <div class="box">
+        <h1>Cadastro</h1>
+        <p>Olá, [Nome]!</p>
+        <p>Por favor, responda às perguntas abaixo para iniciar seu cadastro.</p>
+        <div class="lab-form-item">
+            <p class="lab-form-question">Você está solicitando o auxílio para:</p>           
+            <ul class="lab-form-filter">
+                <li><a href="#home-questions">Espaço Cultural</a></li>
+                <li><a href="#home-questions">Pequena empresa ou coletivo</a></li>
+                <li><a href="<?= $this->controller->createUrl( 'tipo', ['individual'] ) ?>"><?php i::_e('Trabalhador da Cultura') ?></a></li>
+            </ul>
+        </div>
+        <div class="lab-form-item hidden">
+            <p class="lab-form-question">Seu espaço é formalizado?</p>
+            <div class="lab-form-boolean">
+                <span>
+                    <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Sim') ?></a>
+                    <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Não') ?></a>
+                </span>
+                <a href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
+            </div>
+        </div>
+        <div class="lab-form-item hidden">
+            <p class="lab-form-question">Sua pequena empresa ou coletivo é formalizado?</p>
+            <div class="lab-form-boolean">
+                <span>
+                    <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Sim') ?></a>
+                    <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Não') ?></a>
+                </span>
+                <a href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
+            </div>
+        </div>
+    </div>
+</section>
