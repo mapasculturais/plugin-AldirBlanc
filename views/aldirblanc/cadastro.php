be04e2a0382969308e3f91f212f5eb43c7fb803d
Cátia Kitahara <contato@catiakitahara.com.br>
2020-07-31 12:00:28 -0300

    adiciona js nas perguntas

--- views/aldirblanc/cadastro.php
+++ views/aldirblanc/cadastro.php
@@ -2,37 +2,60 @@
 use MapasCulturais\i; 
 
 ?>
+<script>
+    $(document).ready(function(){
+        $('.lab-form-option').addClass('inactive');
+        $('.lab-form-item').hide();
+        $('.lab-form-item:first').show();
+                
+        $('.lab-form-option').click(function(){
+            var t = $(this).attr('id');
+            if($(this).hasClass('inactive')){
+                $('lab-form-option').addClass('inactive');           
+                $(this).removeClass('inactive');
+                
+                $('.lab-form-item').hide();
+                $('#'+ t + 'C').fadeIn('slow');
+            }
+        });
+        $('.back').click(function(){
+            $('.lab-form-option').addClass('inactive');
+            $('.lab-form-item').hide();
+            $('.lab-form-item:first').show();
+        });
+    });
+</script>
 <section class="lab-form-intro">
     <div class="box">
-        <h1>Cadastro</h1>
+        <h1>Cadastro - Primeira tela após login</h1>
         <p>Olá, [Nome]!</p>
         <p>Por favor, responda às perguntas abaixo para iniciar seu cadastro.</p>
         <div class="lab-form-item">
             <p class="lab-form-question">Você está solicitando o auxílio para:</p>           
             <ul class="lab-form-filter">
-                <li><a href="#home-questions">Espaço Cultural</a></li>
-                <li><a href="#home-questions">Pequena empresa ou coletivo</a></li>
-                <li><a href="<?= $this->controller->createUrl( 'tipo', ['individual'] ) ?>"><?php i::_e('Trabalhador da Cultura') ?></a></li>
+                <li><a id="option1" class="lab-form-option" href="#">Espaço Cultural</a></li>
+                <li><a id="option2" class="lab-form-option" href="#">Pequena empresa ou coletivo</a></li>
+                <li><a id="option3" class="lab-form-option" href="<?= $this->controller->createUrl( 'tipo', ['individual'] ) ?>"><?php i::_e('Trabalhador da Cultura') ?></a></li>
             </ul>
         </div>
-        <div class="lab-form-item hidden">
+        <div id="option1C" class="lab-form-item">
             <p class="lab-form-question">Seu espaço é formalizado?</p>
             <div class="lab-form-boolean">
                 <span>
                     <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Sim') ?></a>
                     <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Não') ?></a>
                 </span>
-                <a href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
+                <a class="back" href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
             </div>
         </div>
-        <div class="lab-form-item hidden">
+        <div id="option2C" class="lab-form-item">
             <p class="lab-form-question">Sua pequena empresa ou coletivo é formalizado?</p>
             <div class="lab-form-boolean">
                 <span>
                     <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Sim') ?></a>
                     <a class="btn btn-boolean btn-large" href="<?= $this->controller->createUrl( 'tipo', ['coletivo'] ) ?>"><?php i::_e('Não') ?></a>
                 </span>
-                <a href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
+                <a class="back" href="#"><span class="icon icon-go-back"></span><span class="screen-reader-text">Voltar</span></a>
             </div>
         </div>
     </div>
