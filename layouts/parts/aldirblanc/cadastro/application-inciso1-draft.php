0460a4447f9b618b42e596ffac9a3a568d13a98a
brmagrini@gmail.com <brmagrini@gmail.com>
2020-08-18 11:50:03 -0300

    acerta link no template de inscricoes rascunho #15

--- layouts/parts/aldirblanc/cadastro/application-inciso1-draft.php
+++ layouts/parts/aldirblanc/cadastro/application-inciso1-draft.php
@@ -4,10 +4,10 @@
  * Contém as informações resumidas do cadastro não enviado e link para tela de edição 
  */
 ?>
-<a href="<?=$registrationUrl;?>">
 
-<div id="option1" class="lab-option">
-    <h3>Continuar inscrição iniciada para  <?=$niceName;?></h3>
-    <p class="js-detail lab-option-detail">Sua solicitação do benefício da Renda Emergencial está incompleta. Continue o preenchimento dos campos para finalizá-la.</p>
+<div id="   " class="lab-option">
+    <a href="<?=$registrationUrl;?>">
+        <h3>Continuar inscrição iniciada para  <?=$niceName;?></h3>
+        <p class="js-detail lab-option-detail">Sua solicitação do benefício da Renda Emergencial está incompleta. Continue o preenchimento dos campos para finalizá-la.</p>
+    </a>
 </div>
-<a href="<?=$registrationUrl;?>">
