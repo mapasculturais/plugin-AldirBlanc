4b396bd183416aff8b8d5314ce45de2f64774209
Cátia Kitahara <contato@catiakitahara.com.br>
2020-08-18 10:58:56 -0300

    adiciona classe lab-application-number

--- layouts/parts/aldirblanc/registration-edit--header.php
+++ layouts/parts/aldirblanc/registration-edit--header.php
@@ -1,3 +1,3 @@
 <h1><?php \MapasCulturais\i::_e("Cadastro Lei Aldir Blanc");?></h1>
 
-<p class="textcenter"><?php \MapasCulturais\i::_e("Número da Inscrição");?>: <?php if($action !== 'create'): ?><?php echo $entity->number ?><?php endif; ?></p>
+<p class="lab-application-number"><?php \MapasCulturais\i::_e("Inscrição");?>: <?php if($action !== 'create'): ?><?php echo $entity->number ?><?php endif; ?></p>
