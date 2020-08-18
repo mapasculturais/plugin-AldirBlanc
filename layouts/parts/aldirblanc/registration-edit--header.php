fbd95b3442cb6d80fe2a14bf9275ec81d3d44ac3
Cátia Kitahara <contato@catiakitahara.com.br>
2020-08-17 23:21:55 -0300

    refatora o html e exclui a mensagem sobre campos obrigatórios

---
+++ layouts/parts/aldirblanc/registration-edit--header.php
@@ -0,0 +1,3 @@
+<h1><?php \MapasCulturais\i::_e("Cadastro Lei Aldir Blanc");?></h1>
+
+<p class="textcenter"><?php \MapasCulturais\i::_e("Número da Inscrição");?>: <?php if($action !== 'create'): ?><?php echo $entity->number ?><?php endif; ?></p>
