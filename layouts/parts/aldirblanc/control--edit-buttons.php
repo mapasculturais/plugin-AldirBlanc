130c20666140fae3d1cf5a26e54d3a29f2b0446b
Cátia Kitahara <contato@catiakitahara.com.br>
2020-08-17 23:12:45 -0300

    removi a mensagem de ajuda sobre os ícones de lápis pq não terá mais

--- layouts/parts/aldirblanc/control--edit-buttons.php
+++ layouts/parts/aldirblanc/control--edit-buttons.php
@@ -1,8 +1,1 @@
-
-
-<a class="btn btn-primary js-submit-button"><?php \MapasCulturais\i::_e("Salvar_");?></a>
-
-
-<script type="text/javascript">
-    MapasCulturais.Messages.help('<?php \MapasCulturais\i::esc_attr_e("Os ícones de lápis indicam conteúdos editáveis.");?>');
-</script>
+<a class="btn btn-primary js-submit-button"><?php \MapasCulturais\i::_e("Salvar");?></a>
