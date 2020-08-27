60fb51fb6dd9d3c8f6fbd8ddc3e49f521b0aaceb
brmagrini@gmail.com <brmagrini@gmail.com>
2020-08-27 09:40:19 -0300

    [WiP] - Fluxo de confirmação #38

--- layouts/parts/aldirblanc/registration-edit--validate-button.php
+++ layouts/parts/aldirblanc/registration-edit--validate-button.php
@@ -1,6 +1,21 @@
 <div class="registration-fieldset">
-    <a ng-click="validateRegistration()" class="btn btn-danger hltip" data-hltip-classes="hltip-danger" >Validar</a>
-    
-    <div ng-repeat="field in data.fields" ng-if="entityErrors[field.fieldName]"><a ng-click="scrollTo('wrapper-' + field.fieldName)">{{field.title}}: {{entityErrors[field.fieldName]}}</a></div>
-    
+    <a ng-click="validateRegistration()" class="btn btn-danger">Validar</a>
+    <div ng-repeat="field in data.fields" ng-if="entityErrors[field.fieldName]"><a ng-click="scrollTo('wrapper-' + field.fieldName, 130)">{{field.title}}: <span ng-repeat="error in entityErrors[field.fieldName]">{{error}} </span></a></div>
 </div>
+<div ng-if="entityValidated" style="display:block" id="modalAlert" class="">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Preenchimento Finalizado</h2>
+        <p class="text">lorem</p>
+        <button ng-click="confirma()" class="btn js-confirmar"><?php \MapasCulturais\i::_e("Confirmar"); ?></button>
+    </div>
+</div>
+<script>
+    // $(document).ready(function(){
+
+    // });
+    function confirma(){
+        document.location = MapasCulturais.createUrl('aldirblanc', 'registration-confirmacao',[$scope.data.entity.id])
+    }
+</script>
