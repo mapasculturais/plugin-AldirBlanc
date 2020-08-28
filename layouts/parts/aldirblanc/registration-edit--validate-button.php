c943d09f2f8d0b3cf5a94269156b00bfed9d15b8
Everaldo Matias <matiaseveraldo@gmail.com>
2020-08-28 16:12:14 -0300

    Adiciona modal na validacao do registro de pessoa fisica

--- layouts/parts/aldirblanc/registration-edit--validate-button.php
+++ layouts/parts/aldirblanc/registration-edit--validate-button.php
@@ -7,12 +7,13 @@
         </a>
     </div>
 </div>
-<div ng-if="entityValidated" style="display:block" id="modalAlert" class="">
+<div ng-if="entityValidated" style="display:flex" id="modalAlert" class="modal">
     <!-- Modal content -->
     <div class="modal-content">
         <span class="close">&times;</span>
-        <h2>Preenchimento Finalizado</h2>
-        <p class="text">lorem</p>
+        <h2 class="modal-content--title">Preenchimento Finalizado</h2>
+        <p class="text">Agradecemos sua participação!</p>
+        <p class="text">Antes de enviar a inscrição, releia atentamente os dados preenchidos e certifique-se que estão todos corretos. Você pode editar o formulário caso encontre alguma informação incorreta.</p>
         <a href="<?= $this->controller->createUrl('confirmacao', [$entity->id]) ?>" ng-click="" class="btn btn-primary js-confirmar"><?php \MapasCulturais\i::_e("Confirmar"); ?></a>
     </div>
 </div>
