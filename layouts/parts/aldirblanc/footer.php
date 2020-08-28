ff1c9706175249ddc0db23ddd5674585f673933d
Everaldo Matias <matiaseveraldo@gmail.com>
2020-08-28 00:37:33 -0300

    Adiciona rodape

--- layouts/parts/aldirblanc/footer.php
+++ layouts/parts/aldirblanc/footer.php
@@ -1,5 +1,21 @@
+    <div class="push"></div>
 </section>
-<footer id="main-footer"></footer>
+<footer id="main-footer">
+
+    <div class="logo-state">
+        <img src="<?php $this->asset('aldirblanc/img/logo-sergipe.png') ?>">
+    </div>
+
+    <div class="credits">
+        <img alt="Mapas Culturais e hacklab/" src="<?php $this->asset('aldirblanc/img/mapas-culturais-hacklab.png') ?>">
+    </div>
+
+</footer>
+
+<div class="support">
+    <a href="">Suporte</a>
+</div>
+
 <?php $this->bodyEnd(); ?>
 </body>
 
