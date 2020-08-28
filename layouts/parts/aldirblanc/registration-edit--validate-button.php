9a16fb80180fc60aa1e9e81da74b3a26c114650a
Everaldo Matias <matiaseveraldo@gmail.com>
2020-08-28 14:32:28 -0300

    Ajuste no alinhamento e fontes do formulario de edicao

--- layouts/parts/aldirblanc/registration-edit--validate-button.php
+++ layouts/parts/aldirblanc/registration-edit--validate-button.php
@@ -1,8 +1,8 @@
 <div class="registration-fieldset">
-    <a ng-click="validateRegistration()" class="btn btn-danger">Validar</a>
+    <a ng-click="validateRegistration()" class="btn btn-secondary">Validar</a>
     <div ng-repeat="field in data.fields" ng-if="entityErrors[field.fieldName]">
         <a ng-click="scrollTo('wrapper-' + field.fieldName, 130)">
-            {{field.title}}: 
+            {{field.title}}:
             <span ng-repeat="error in entityErrors[field.fieldName]">{{error}} </span>
         </a>
     </div>
@@ -13,6 +13,6 @@
         <span class="close">&times;</span>
         <h2>Preenchimento Finalizado</h2>
         <p class="text">lorem</p>
-        <a href="<?= $this->controller->createUrl('confirmacao',[$entity->id]) ?>" ng-click="" class="btn btn-primary js-confirmar"><?php \MapasCulturais\i::_e("Confirmar"); ?></a>
+        <a href="<?= $this->controller->createUrl('confirmacao', [$entity->id]) ?>" ng-click="" class="btn btn-primary js-confirmar"><?php \MapasCulturais\i::_e("Confirmar"); ?></a>
     </div>
 </div>
