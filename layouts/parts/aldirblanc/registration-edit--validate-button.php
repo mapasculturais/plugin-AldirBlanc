b92b9c51fc9e666bb633213dfa3dd2c0bd4c859b
Tharles de Sousa Andrade <irtharles@gmail.com>
2020-08-31 23:58:15 -0300

    Ajuste no css do formulário para não quebrar no mobile.

--- layouts/parts/aldirblanc/registration-edit--validate-button.php
+++ layouts/parts/aldirblanc/registration-edit--validate-button.php
@@ -1,10 +1,9 @@
 <div class="registration-fieldset">
     <a ng-click="validateRegistration()" class="btn btn-secondary">Validar</a>
-
-    <div class="errors-header"ng-repeat="field in data.fields" ng-if="entityErrors[field.fieldName] && $first">
-        <p class="errors-header-title">O cadastro não foi enviado!</p>
-        <p>Corrija os campos e envie novamente</p>
-    </div>
+<!--    <div class="errors-header" ng-if="field.id == data.fields[0].id">-->
+<!--        <p class="errors-header-title">O cadastro não foi enviado!</p>-->
+<!--        <p >Corrija os campos e envie novamente</p>-->
+<!--    </div>-->
 
     <div class="errors" ng-repeat="field in data.fields" ng-if="entityErrors[field.fieldName]">
         <a ng-click="scrollTo('wrapper-' + field.fieldName, 130)">
