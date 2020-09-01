1b8b662ac5e45cb3c96d2b146caf49fd6c240f9e
Ben Rainir <benrainir@gmail.com>
2020-09-01 09:40:14 -0300

    Corrigindo texto da home sobre a Lei Aldir Blanc

--- layouts/parts/aldirblanc/home-search.php
+++ layouts/parts/aldirblanc/home-search.php
@@ -1,6 +1,10 @@
 <div class="box">
     <h1>Lei Aldir Blanc</h1>
-    <p>A Lei de Emergência Cultural Aldir Blanc surgiu com o objetivo de ajudar os trabalhadores da Cultura e os espaços culturais brasileiros no período de isolamento social, ocasionado pela pandemia da COVID-19</p>
+    <p>
+        A Lei Aldir Blanc é fruto de forte mobilização social do campo artístico e cultural brasileiro, resultado de construção coletiva, a partir de webconferências nacionais e estaduais como plataformas políticas na formulação, articulação, tramitação e sanção presidencial.<br/>
+        Ela prevê o uso de 3 bilhões de reais para o auxílio de agentes da cultura atingidos pela pandemia da COVID-19. Investimentos para assegurar a preservação de toda a estrutura profissional e dinâmica de produção, criação, participação, preservação, formação e circulação dos bens e serviços culturais.<br/>
+        Clique no link abaixo para solicitar a renda emergencial como trabalhadora e trabalhador da cultura ou o subsídio para a manutenção de espaços artísticos e organizações culturais que tiveram as suas atividades interrompidas por força das medidas de isolamento social.
+    </p>
     
     <a class="btn btn-accent btn-large" href="<?php echo $app->createUrl('aldirblanc') ?>">Solicite seu auxilio</a>
 </div>
