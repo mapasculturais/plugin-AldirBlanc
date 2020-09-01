7dbf705df3eb8dac76b7344b40cde9d7ecb215e9
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-08-31 21:33:09 -0300

    remove logo sergipe do header

--- layouts/parts/aldirblanc/header-logos.php
+++ layouts/parts/aldirblanc/header-logos.php
@@ -5,7 +5,7 @@
 ?>
 
 <div class="logo-state">
-    <img src="<?php $this->asset('aldirblanc/img/logo-sergipe.png') ?>">
+    <img src="<?php //$this->asset('aldirblanc/img/logo-sergipe.png') ?>">
 </div>
 
 <div class="logo">
