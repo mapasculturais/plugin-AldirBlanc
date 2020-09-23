de9a3660039f5a14fadc0823a7032129bfb8863e
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-09-22 22:17:00 -0300

    registra a role mediador (refs: #93)

--- Plugin.php
+++ Plugin.php
@@ -217,6 +217,10 @@ class Plugin extends \MapasCulturais\Plugin
 
         $app->registerController('aldirblanc', 'AldirBlanc\Controllers\AldirBlanc');
 
+        // registra o role para mediadores
+        $role_definition = new Role('mediador', 'Mediador', 'Mediadores', true, function($user){ return $user->is('admin'); });
+        $app->registerRole($role_definition);
+
         /* registrinado metadados do usuário */
 
         /**
