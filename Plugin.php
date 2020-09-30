0805b0127620b7df85d09d1c1fd6e2edf275c8f3
brmagrini@gmail.com <brmagrini@gmail.com>
2020-09-30 02:36:30 -0300

    adiciona itens padrao da home na config do plugin

--- Plugin.php
+++ Plugin.php
@@ -22,6 +22,9 @@ class Plugin extends \MapasCulturais\Plugin
         }
 
         $config += [
+            'texto_home'=> env('AB_TEXTO_HOME','A Lei Aldir Blanc é fruto de forte mobilização social do campo artístico e cultural brasileiro, resultado de construção coletiva, a partir de webconferências nacionais e estaduais como plataformas políticas na formulação, articulação, tramitação e sanção presidencial.<br/><br/> Ela prevê o uso de 3 bilhões de reais para o auxílio de agentes da cultura atingidos pela pandemia da COVID-19. Investimentos para assegurar a preservação de toda a estrutura profissional e dinâmica de produção, criação, participação, preservação, formação e circulação dos bens e serviços culturais.<br/><br/> Clique no link abaixo para solicitar a renda emergencial como trabalhadora e trabalhador da cultura ou o subsídio para a manutenção de espaços artísticos e organizações culturais que tiveram as suas atividades interrompidas por força das medidas de isolamento social.'),
+            'botao_home'=> env('AB_BOTAO_HOME','Solicite seu auxilio'),
+            'titulo_home'=> env('AB_TITULO_HOME','Lei Aldir Blanc'),
             'logotipo_central' => env('AB_LOGOTIPO_CENTRAL',''),
             'logotipo_instituicao' => env('AB_LOGOTIPO_INSTITUICAO',''),
             'inciso1_enabled' => env('AB_INCISO1_ENABLE',true),
