3d48efd26b49720f25262dcfe5eb2f58c51af9bd
brmagrini@gmail.com <brmagrini@gmail.com>
2020-10-02 20:19:16 -0300

    envia opportunidade pára o botão de exportação #97

--- layouts/parts/aldirblanc/csv-button.php
+++ layouts/parts/aldirblanc/csv-button.php
@@ -1,6 +1,22 @@
 <?php 
-$route = MapasCulturais\App::i()->createUrl('dataprev', 'export');
+if ($inciso == 1){
+    $route = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso1');    
+    ?>
+    <a class="btn btn-primary" href="<?= $route ?>" target="_blank">
+        Exportar csv
+    </a>
+    <?php
+}
+else if ($inciso ==2){
+    $routeCpf = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso1', ['status' => 1, 'type' =>'cpf', 'opportunity' =>$opportunity]);
+    $routeCnpj = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso1', ['status' => 1, 'type' =>'cnpj', 'opportunity' =>$opportunity]);
+    ?>
+    <a class="btn btn-primary" href="<?= $routeCpf ?>" target="_blank">
+        Exportar csv CPF
+    </a>
+    <a class="btn btn-primary" href="<?= $routeCnpj ?>" target="_blank">
+        Exportar csv CNPJ
+    </a>
+    <?php
+}
 ?>
-<a class="btn btn-primary" href="<?= $route ?>" target="_blank">
-    Exportar csv
-</a>
