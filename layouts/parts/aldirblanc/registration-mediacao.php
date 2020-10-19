902682a055faaeaf6abdabda3c8cbb20aa82795f
Everaldo Matias <matiaseveraldo@gmail.com>
2020-10-19 16:43:49 -0300

    CSS para adaptar o campo de Documentacao para a mediacao, na pagina de confirmacao, com o layout do site

--- layouts/parts/aldirblanc/registration-mediacao.php
+++ layouts/parts/aldirblanc/registration-mediacao.php
@@ -1,31 +1,55 @@
-<div class="registration-fieldset card registration-fieldset-mediacao">
+<?php
 
-    <h4>Informações por Mediação</h4>
+$mediacao_contato_tipo = '';
 
-    <p>
-        <strong>Autorização: </strong>
+switch ($entity->mediacao_contato_tipo) {
+    case 'telefone-fixo':
+        $mediacao_contato_tipo = 'Telefone fixo';
+        break;
+
+    case 'whatsapp':
+        $mediacao_contato_tipo = 'WhatsApp';
+        break;
+
+    case 'SMS':
+        $mediacao_contato_tipo = 'SMS';
+        break;
+    
+    default:
+        $mediacao_contato_tipo = '--';
+        break;
+} ?>
+
+<div class="registration-fieldset registration-fieldset-moderator">
+
+    <div class="each-line">
+        <span class="label">Documentação para a mediação</span>
+    </div>
+
+    <div class="each-line">
+        <span class="label">Autorização</span>
         <?php if (isset($entity->files['mediacao-autorizacao'][0])) : ?>
             <a class="attachment-title" href="<?php echo $entity->files['mediacao-autorizacao'][0]->getUrl(); ?>" target="_blank" rel="noopener noreferrer"><?php echo basename($entity->files['mediacao-autorizacao'][0]->getPath()); ?></a>
         <?php else : ?>
-            <span>Arquivo não enviado</span>
+            <span><em>Arquivo não enviado</em></span>
         <?php endif; ?>
-    </p>
-    <p>
-        <strong>Documento (RG): </strong>
+    </div>
+    <div class="each-line">
+        <span class="label">Documento (RG)</span>
         <?php if (isset($entity->files['mediacao-documento'][0])) : ?>
             <a class="attachment-title" href="<?php echo $entity->files['mediacao-documento'][0]->getUrl(); ?>" target="_blank" rel="noopener noreferrer"><?php echo basename($entity->files['mediacao-documento'][0]->getPath()); ?></a>
         <?php else : ?>
-            <span>Arquivo não enviado</span>
+            <span><em>Arquivo não enviado</em></span>
         <?php endif; ?>
-    </p>
-    <p>
-        <strong>O contato será por: </strong>
-        <?php echo (isset($entity->mediacao_contato_tipo)) ? '--' : $entity->mediacao_contato_tipo; ?>
-    </p>
+    </div>
+    <div class="each-line">
+        <span class="label">O contato será por</span>
+        <span><?php echo $mediacao_contato_tipo; ?></span>
+    </div>
 
-    <p>
-        <strong>Telefone: </strong>
-        <?php echo (isset($entity->mediacao_contato)) ? '--' : $entity->mediacao_contato; ?>
-    </p>
+    <div class="each-line">
+        <span class="label">Telefone</span>
+        <span><?php echo $entity->mediacao_contato; ?></span>
+    </div>
 
 </div>
