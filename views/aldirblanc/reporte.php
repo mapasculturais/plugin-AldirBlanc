4886d6e7a1ba6483d34ef8c03e90e021f9efb62a
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-11-11 18:14:16 -0300

    novas tabelas na página de reporte

--- views/aldirblanc/reporte.php
+++ views/aldirblanc/reporte.php
@@ -13,7 +13,32 @@
     </section>
 <?php endif ?>
 
-<?php if ($inciso2) : ?>
+<?php 
+if ($inciso2) : 
+    $controller = $app->controller('Opportunity');
+?>
+<style>
+    .oportunidades .report-item {
+        display: inline-block;
+    }
+    .oportunidades th {
+        text-align: left;
+    }
+    .oportunidades td {
+        text-align: left;
+        padding: 0 1em;
+    }
+
+    .oportunidades td.number {
+        text-align: center;
+    }
+
+    .oportunidades tr:hover {
+        background-color: #fff;
+    }
+    
+    
+</style>
     <section class="lab-main-content cadastro">
         <header>
             <div class="intro-message">
@@ -25,5 +50,58 @@
             <h2 class="featured-title">
                 <?= $inciso2->total ?> benefícios solicitados.
             </h2>
+
+            <section class="oportunidades">
+                <div class="report-item">
+                    <h3>Oportunidades com inscrições enviadas</h3>
+                    <table>
+                        <tr>
+                            <th>Oportunidade</th>
+                            <th>Inscrições</th>
+                        </tr>
+                        <?php foreach($inciso2->enviadas as $opp): $opp = (object) $opp; ?>
+                            <tr>
+                                <td>
+                                    <a href="<?= $controller->createUrl('single', [$opp->id]) ?>"><?= $opp->name ?></a>
+                                </td>
+                                <td class='number'><?= $opp->num_inscricoes ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <div class="report-item">
+                    <h3>Oportunidades somente com inscrições rascunho</h3>
+                    <table style="width: 80%;">
+                        <tr>
+                            <th>Oportunidade</th>
+                            <th>Inscrições</th>
+                        </tr>
+                        <?php foreach($inciso2->soh_rascunhos as $opp): $opp = (object) $opp; ?>
+                            <tr>
+                                <td>
+                                    <a href="<?= $controller->createUrl('single', [$opp->id]) ?>"><?= $opp->name ?></a>
+                                </td>
+                                <td class='number'><?= $opp->num_inscricoes ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+
+                <div class="report-item">
+                    <h3>Oportunidades sem nenhuma inscrição</h3>
+                    <table style="width: 80%;">
+                        <tr>
+                            <th>Oportunidade</th>
+                        </tr>
+                        <?php foreach($inciso2->sem_inscricao as $opp): $opp = (object) $opp; ?>
+                            <tr>
+                                <td>
+                                    <a href="<?= $controller->createUrl('single', [$opp->id]) ?>"><?= $opp->name ?></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+            </section>
     </section>
 <?php endif ?>
