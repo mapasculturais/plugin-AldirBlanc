f9651fee53d22e4c943c1405bdd6d27501dce3b9
Willow <brmagrini@gmail.com>
2020-11-25 10:03:37 -0300

    altera linha em branco

--- Plugin.php
+++ Plugin.php
@@ -21,6 +21,7 @@ class Plugin extends \MapasCulturais\Plugin
             $config = $app->view->subsite->aldir_blanc_config;
         }
 
+
         $config += [
             'texto_home'=> env('AB_TEXTO_HOME','A Lei Aldir Blanc é fruto de forte mobilização social do campo artístico e cultural brasileiro, resultado de construção coletiva, a partir de webconferências nacionais e estaduais como plataformas políticas na formulação, articulação, tramitação e sanção presidencial.<br/><br/> Ela prevê o uso de 3 bilhões de reais para o auxílio de agentes da cultura atingidos pela pandemia da COVID-19. Investimentos para assegurar a preservação de toda a estrutura profissional e dinâmica de produção, criação, participação, preservação, formação e circulação dos bens e serviços culturais.<br/><br/> Clique no link abaixo para solicitar a renda emergencial como trabalhadora e trabalhador da cultura ou o subsídio para a manutenção de espaços artísticos e organizações culturais que tiveram as suas atividades interrompidas por força das medidas de isolamento social.'),
             'botao_home'=> env('AB_BOTAO_HOME','Solicite seu auxilio'),
