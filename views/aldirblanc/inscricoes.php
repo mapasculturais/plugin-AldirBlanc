5d6908d65de0d3263bc45e7c58fc0094f5218ecb
erleibiazzio <erleibiazzio@gmail.com>
2020-12-11 08:56:29 -0300

    Atualiza tabela de visualizçõa de inscrições

--- views/aldirblanc/inscricoes.php
+++ views/aldirblanc/inscricoes.php
@@ -1,10 +1,12 @@
 <section id="lab-status" class="lab-main-content">
     <article class="main-content registration" ng-controller="OpportunityController">
-        <table>
+        <table class ="js-registration-list registrations-table no-options">
             <tr>
                 <td>ID OPORTUNIDADE</td>
                 <td>NOME DA OPORTUNIDADE</td>
                 <td>QUANTIDADE</td>
+                <td>CPF</td>
+                <td>CNPJ</td>
             </tr>
 
             <?php foreach ($opportunities as $key => $value) { ?>
@@ -12,6 +14,8 @@
                     <td><?=$value['id']?></td>
                     <td><?=$value['name']?></td>
                     <td><?=$value['num']?></td>
+                    <td><a href="/dataprev/export_inciso2/opportunity:<?=$value['id']?>/type:cpf">CPF</a></td>
+                    <td><a href="/dataprev/export_inciso2/opportunity:<?=$value['id']?>/type:cnpj">CNPJ</a></td>
                 </tr>
             <?php  }?>
         </table>
