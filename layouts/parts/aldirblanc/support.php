aacaf13eaa58d834b35ef3652842b5277290ac50
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2020-12-24 15:37:32 -0300

    cor do link do texto de suporte

--- layouts/parts/aldirblanc/support.php
+++ layouts/parts/aldirblanc/support.php
@@ -4,6 +4,9 @@
         text-align: center;
         color:black;
     }
+    #main-footer .support a, #main-footer .support a:hover {
+        color: black;
+    }
     </style>
     <div class="support help-section">
         Precisa de ajuda? <a href="https://bit.ly/3hOQfBz" target="_blank"> Clique aqui </a> para falar com nossa equipe de suporte por chat. Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com" target="_blank"> suportemapaculturalpa@gmail.com </a>
