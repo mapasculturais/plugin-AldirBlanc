519b5217ce7cc7818d38094cd0e3be3da87a6345
Erlei Biazzio <erleibiazzio@gmail.com>
2021-01-26 09:19:24 -0300

    Separa hooks de observações da tela de pagamento

---
+++ layouts/parts/aldirblanc/observacoes-pagamento-edit-single.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RegistrationPayments;
+
+use MapasCulturais\i;
+
+?>
+
+<label><span class="label"> <?php i::_e("Observações"); ?></span>
+    <textarea ng-model="data.editPayment.metadata.csv_line.OBSERVACOES" cols="90" rows="5"></textarea>
+</label>
