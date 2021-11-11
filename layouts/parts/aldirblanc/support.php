cf84abb4ed6608a6027933cdecc56b764c321983
erleibiazzio <erleibiazzio@gmail.com>
2021-11-11 10:01:06 -0300

    Insere opção de setar email de suporte via variável de ambiente

--- layouts/parts/aldirblanc/support.php
+++ layouts/parts/aldirblanc/support.php
@@ -9,5 +9,5 @@
     }
     </style>
     <div class="support help-section">
-        Precisa de ajuda? <a href="https://bit.ly/3hOQfBz" target="_blank"> Clique aqui </a> para falar com nossa equipe de suporte por chat. Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com" target="_blank"> suportemapaculturalpa@gmail.com </a>
+        Precisa de ajuda? <a href="https://bit.ly/3hOQfBz" target="_blank"> Clique aqui </a> para falar com nossa equipe de suporte por chat. Ou envie um email para <a href="mailto:<?=env('PA_SUPORT_EMAIL', 'suportemapaculturalpa@gmail.com')?>" target="_blank"><?=env('PA_SUPORT_EMAIL', 'suportemapaculturalpa@gmail.com')?></a>
     </div>
