93022f025de1e0378ce2fe8e388e1390daf70d61
erleibiazzio <erleibiazzio@gmail.com>
2022-02-10 18:09:53 -0300

    Passa texto para configuração

--- layouts/parts/aldirblanc/home-search.php
+++ layouts/parts/aldirblanc/home-search.php
@@ -1,16 +1,16 @@
 
 <div class="box">
     <h1><?= $titulo ?></h1>
-    <p style="text-align:justify">
     <div style="text-align:center">
         <img src="<?=$this->asset('aldirblanc/img/preamar1.png', false)?>" alt="Preamar de cultura e arte" style="width: 15%; margin-right: 10px;">
         <img src="<?=$this->asset('aldirblanc/img/preamar2.png', false)?>" alt="Preamar da leitura" style="width: 15%;">
-    </div>
-        
-        Já estão abertas as inscrições para os editais Preamar da Leitura e Preamar de Cultura e Arte. O primeiro vai credenciar até 200 bibliotecas públicas do Estado para fins de recebimento de <br><br> 
-        doação de livros. Já o segundo, destina-se à distribuição de 5 milhões de reais, que premiará mais de 200 propostas culturais inéditas em diversas linguagens, expressões, 
-        práticas, <br><br>manifestações e segmentos culturais do Estado. Inscreva-se pelo Mapa Cultural. Não perca essa chance e vem realizar com a gente!
-    </p>
+    </div >  
+    <br><br>
+   <div>
+        <p style="text-align:center; line-height:27px;">
+            <?=$texto?>
+        </p>
+   </div>
     
     <!-- <a class="btn btn-accent btn-large" href="<?php echo $app->createUrl('aldirblanc') ?>"><?= $botao ?></a> -->
 </div>
